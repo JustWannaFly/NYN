be2440030e91e1fddb0d9b31f7de39e7925448d3
JustWannaFly <31224074+JustWannaFly@users.noreply.github.com>
2022-01-31 01:59:48 -0600

    render blank window

--- src/NYN.cpp
+++ src/NYN.cpp
@@ -12,8 +12,19 @@ int main(int argc, char* args[]) {
 	SDL_Surface* screenSurface = NULL;
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+	} else {
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL) {
+			std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		} else {
+			screenSurface = SDL_GetWindowSurface(window);
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface(window);
+			SDL_Delay(2000);
+		}
 	}
 
-	std::cout << "Hello CMake." << std::endl;
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
